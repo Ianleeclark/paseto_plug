a3a922210d0116c12100f7d2607da87ad0937b7d
Ian Clark <ian@ianleeclark.com>
2018-06-27 19:33:00 -0500

    Filling out all invalid key test

--- test/paseto_plug_test.exs
+++ test/paseto_plug_test.exs
@@ -31,7 +31,18 @@ defmodule PasetoPlugTest do
     end
   end
 
-  defmodule TestRouterPrivate do
+  defmodule TestRouterPublicInvalid do
+    import Plug.Conn
+    use Plug.Router
+
+    plug PasetoPlug, key_provider: fn -> :crypto.generate_key(:rsa, {2048, 65_637}) end
+
+    get "/" do
+      send_resp(conn, 200, "Paseto verified")
+    end
+  end
+
+  defmodule TestRouterLocal do
     import Plug.Conn
     use Plug.Router
 
@@ -44,13 +55,50 @@ defmodule PasetoPlugTest do
     end
   end
 
+  defmodule TestRouterLocalInvalid do
+    import Plug.Conn
+    use Plug.Router
+
+    plug PasetoPlug, key_provider: fn -> "invalid_key" end
+
+    get "/" do
+      send_resp(conn, 200, "Paseto verified")
+    end
+  end
+
+  describe "V1 Plug Tests" do
+    test "Invalid V1 local token" do
+      conn =
+        conn(:get, "/")
+        |> put_req_header("authorization", "Bearer v1.local.VGhpcyBpcyBhIHRlc3QgbWVzc2FnZSe-sJyD2x_fCDGEUKDcvjU9y3jRHxD4iEJ8iQwwfMUq5jUR47J15uPbgyOmBkQCxNDydR0yV1iBR-GPpyE-NQw")
+
+      assert TestRouterLocalInvalid.call(conn, []).status == 401
+    end
+
+    test "Invalid V1 public token" do
+      conn =
+        conn(:get, "/")
+        |> put_req_header("authorization", "Bearer v1.local.VGhpcyBpcyBhIHRlc3QgbWVzc2FnZSe-sJyD2x_fCDGEUKDcvjU9y3jRHxD4iEJ8iQwwfMUq5jUR47J15uPbgyOmBkQCxNDydR0yV1iBR-GPpyE-NQw")
+
+      assert TestRouterPublicInvalid.call(conn, []).status == 401
+    end
+  end
+
   describe "V2 Plug Tests" do
     test "Invalid V2 local token" do
       conn =
         conn(:get, "/")
         |> put_req_header("authorization", "Bearer v2.local.VGhpcyBpcyBhIHRlc3QgbWVzc2FnZSe-sJyD2x_fCDGEUKDcvjU9y3jRHxD4iEJ8iQwwfMUq5jUR47J15uPbgyOmBkQCxNDydR0yV1iBR-GPpyE-NQw")
 
-      assert TestRouterPublic.call(conn, []).status == 401
+      assert TestRouterLocalInvalid.call(conn, []).status == 401
+    end
+
+    test "Invalid V2 public token" do
+      conn =
+        conn(:get, "/")
+        |> put_req_header("authorization", "Bearer v2.local.VGhpcyBpcyBhIHRlc3QgbWVzc2FnZSe-sJyD2x_fCDGEUKDcvjU9y3jRHxD4iEJ8iQwwfMUq5jUR47J15uPbgyOmBkQCxNDydR0yV1iBR-GPpyE-NQw")
+
+      assert TestRouterPublicInvalid.call(conn, []).status == 401
     end
   end
 end
