ddfd8c2187b997120ffa8194868a51451de27a6b
Ian Clark <ian@ianleeclark.com>
2019-06-06 22:33:02 +0200

    Upgrading to paseto 1.3
    asdf

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule PasetoPlug.MixProject do
   def project do
     [
       app: :paseto_plug,
-      version: "0.3.0",
-      elixir: "~> 1.7",
+      version: "0.4.0",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
@@ -22,11 +22,11 @@ defmodule PasetoPlug.MixProject do
 
   defp deps do
     [
-      {:paseto, "~> 1.2"},
+      {:paseto, "~> 1.3"},
       {:plug, "~> 1.0"},
       # Non-core dependencies, but nice-to-have
       {:credo, "~> 0.9.1", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.16", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
     ]
   end
 
